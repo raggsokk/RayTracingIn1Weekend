16cf4d2a737d0e7fe3079d33e4479d39b0f475a2
Jarle Hansen <jarle.hansen@gmail.com>
2019-06-04 00:13:15 +0200

    Added initial Ray.cs

---
+++ src/RayTracingIn1Weekend/Week1/Ray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace RayTracingIn1Weekend.Week1
+{
+    [StructLayout(LayoutKind.Sequential)]
+    [DebuggerDisplay("")]
+    public struct Ray
+    {
+        public Vec3f Origin;
+        public Vec3f Direction;
+
+        public Ray(Vec3f o, Vec3f d)
+        {
+            this.Origin = o;
+            this.Direction = d;
+        }
+
+        public Vec3f PointAtParameter(float f) => this.Origin + f * this.Direction;
+    }
+}
