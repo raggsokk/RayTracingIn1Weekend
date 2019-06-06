db820d1412dda28d36bf701e0c02cc2250a3ba7f
Jarle Hansen <jarle.hansen@gmail.com>
2019-06-07 00:51:59 +0200

    Fixed red blue sphere image. (chapter 10).

--- src/RayTracingIn1Weekend/Week1/Camera.cs
+++ src/RayTracingIn1Weekend/Week1/Camera.cs
@@ -11,12 +11,21 @@ namespace RayTracingIn1Weekend.Week1
         public Vec3f Horizontal;
         public Vec3f Vertical;
 
-        public Camera()
+        public Camera(float vfov, float aspect)
         {
-            LowerLeftCorner = new Vec3f(-2.0f, -1.0f, -1.0f);
-            Horizontal = new Vec3f(4.0f, 0.0f, 0.0f);
-            Vertical = new Vec3f(0.0f, 2.0f, 0.0f);
+            float theta = vfov * MathF.PI / 180;
+            float half_height = MathF.Tan(theta / 2);
+            float half_width = aspect * half_height;
+
+            LowerLeftCorner = new Vec3f(-half_width, -half_height, -1.0f);
+            Horizontal = new Vec3f(2 * half_width, 0f, 0f);
+            Vertical = new Vec3f(0f, 2 * half_height, 0f);
             Origin = Vec3f.Zero;
+
+            //LowerLeftCorner = new Vec3f(-2.0f, -1.0f, -1.0f);
+            //Horizontal = new Vec3f(4.0f, 0.0f, 0.0f);
+            //Vertical = new Vec3f(0.0f, 2.0f, 0.0f);
+            //Origin = Vec3f.Zero;
         }
 
         public Ray GetRay(float u, float v)
