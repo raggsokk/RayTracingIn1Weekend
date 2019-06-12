e263ae143e896f5f6a45c606b131f5380b4036a5
Jarle Hansen <jarle.hansen@gmail.com>
2019-06-12 13:04:38 +0200

    Added yet another argument parser.

---
+++ src/RayTracingConsole/Options.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace RayTracingConsole
+{
+    public class Options
+    {
+        public int Width {get;set;}
+        public int Height {get;set;}
+        public int Samples {get;set;}
+
+        public string Output {get;set;}
+    }
+}
